b2fb58e1c8700847757a4d03f4cdc9759b3ba185
Philip Graham <Philip Graham>
2016-10-22 09:13:02 -0400

    Week 11 Constructor Homework :|

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Constructors
+
+Constructors are especially useful in creating and using Objects. Javascript alone uses single objects, but Constructors give the programmer the power to create an object “type” that can be used as a template of sorts for creating many objects of that “type”:
+
+function person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+var myFather = new person("John", "Doe", 50, "blue");
+var myMother = new person("Sally", "Rally", 48, "green");
+
+In the above example, “person” is the Object constructor, and myFather and myMother are “new” objects of the same Object “type.”
+
+The “this” keyword is the Object that owns the Javascript code. “This” is not a variable – it can only be used as a specific value:
+
+When used in a function, it is the Object that “owns” the function.
+When used in an object, it is the Object.
+
+The intention of this homework was to use Constructors to change a previous assignment from vanilla Javascript to Javascript with Constructors, giving the student an understanding of the utility and power of Constructors.
---
+++ game.js
@@ -0,0 +1,138 @@
+// Global Variables{{
+// *********************************************
+
+function Bands (aerosmith, mountain, queen, skylark, argent, trex) {
+	this.aerosmith = aerosmith;
+	this.mountain = mountain;
+	this.queen = queen;
+	this.skylark = skylark;
+	this.argent = argent;
+
+}
+
+var selectedWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongLetters = [];
+
+// Game Counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
+
+// Functions
+//**********************************************
+
+function startGame () {
+	selectedWord = Bands[Math.floor(Math.random()*wordOptions.length)];
+	lettersinWord = selectedWord.split("");
+	numBlanks = lettersinWord.length;
+
+	//Reset counters
+	guessesLeft = 9;
+	wrongLetters = [];
+	blanksAndSuccesses = [];
+
+	for (var i=0; i<numBlanks; i++){
+		blanksAndSuccesses.push("_");
+	}
+	
+	//Change HTML to reflect round condition
+	document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join("  ");
+	document.getElementById("numGuesses").innerHTML = guessesLeft;
+	document.getElementById("winCounter").innerHTML = winCount;
+	document.getElementById("lossCounter").innerHTML = lossCount;
+
+}
+
+function checkLetters()
+
+function checkLetters(letter) {
+//Check for letter
+
+	var isLetterInWord  = false;
+
+	for (var i=0; i<numBlanks; i++) {
+		if(selectedWord[i] == letter) {
+			isLetterInWord = true;
+		}
+	}
+
+	//Confirm letter location and populate
+	if (isLetterInWord) {
+		for (var i=0; i<numBlanks; i++) {
+			if(selectedWord[i] == letter) {
+				blanksAndSuccesses[i] =	 letter;
+			}
+		}
+	}
+
+	//Letter not found
+	else {
+		wrongLetters.push(letter);
+		guessesLeft--
+		}
+}	
+
+function roundComplete() {
+	console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " | Guesses Left " + guessesLeft);
+
+	//Update the HTML to reflect current count stats
+	document.getElementById("numGuesses").innerHTML = guessesLeft;
+	document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join("  ");
+	document.getElementById("wrongGuesses").innerHTML = wrongLetters.join("  ");
+
+	//Check if win :)
+	if (lettersinWord.toString() == blanksAndSuccesses.toString()) {
+		winCount++; 
+		alert("Far out! You won, cool cat (if male) fine lady (if female)");
+											
+		//Update HTML win counter
+		document.getElementById("winCounter").innerHTML = winCount;
+			
+		startGame();
+	}
+
+	//Check if lose :(
+	else if (guessesLeft == 0) {
+		lossCount++;
+		alert("Heavy scene, man. You lose.");
+
+		//Update HTML loss counter
+		//Update HTML win counter
+		document.getElementById("lossCounter").innerHTML = lossCount;
+
+		startGame();
+	}
+}
+
+//function myFunction() {
+	//	    var x = document.createElement("AUDIO");
+
+	//	    if (x.canPlayType("audio/mpeg")) {
+	//	        x.setAttribute("src","http://jugnifm.com/music/temp320/Mississippi_Queen_-_[JugniFm.CoM].mp3yourAudioTag");
+	//	    } 
+	//	    x.setAttribute("controls", "controls");
+	//	    document.body.appendChild(x);
+		
+//function updateSource(){ 
+//    var audio = document.getElementById('http://jugnifm.com/music/temp320/Mississippi_Queen_-_[JugniFm.CoM].mp3yourAudioTag');
+//        audio.load(); 
+//}	
+
+
+// Main Process
+//**********************************************
+
+//Game start//	
+ startGame();
+
+document.onkeyup = function (event) {
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	checkLetters(letterGuessed);
+	roundComplete();
+	//myFunction();
+	//updateSource();
+	//Testing this code//
+}
